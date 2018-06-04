c6c3ec0fdf8564fca5b1532334e3df82bf3c5ff4
Yurina Shin <yurinas@gmail.com>
2018-06-04 21:29:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,7 +8,7 @@ class Student
       @name = student_hash[:name]
       @location = student_hash[:location]
       @profile_url = student_hash[:profile_url]
-      binding.pry
+      # binding.pry
       @@all << self
   end
 
